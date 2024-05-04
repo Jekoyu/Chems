38c726434675498b447a536f09dc0dacc98a4a8f
Joko Yuliyanto <46364980+Jekoyu@users.noreply.github.com>
2024-05-04 23:50:57 +0700

    Initial commit

---
+++ database/migrations/2024_05_04_162535_belanja.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('belanja', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->unsignedBigInteger('id_produk');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('id_produk')->references('id')->on('produk')->onDelete('cascade');
+            $table->integer('total_harga');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('belanja');
+    }
+};
